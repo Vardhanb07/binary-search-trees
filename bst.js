2d47a7b1585e291e0711fb64736ba93d9a235f59
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-06-07 11:28:33 +0530

    Add buildTree

--- bst.js
+++ bst.js
@@ -1,22 +1,50 @@
+import { mergeSort } from "./mergeSort.js"
 class Node {
-    constructor(data, left, right) {
+    constructor(data) {
         this.data = data
-        this.left = left
-        this.right = right
+        this.left = null
+        this.right = null
     }
 }
-class Tree {
-    constructor(array, root = 0) {
-        array.sort()
-        let sortedSet = new Set([])
+export class Tree {
+    constructor(array, root) {
+        this.array = []
+        let set = new Set([])
         array.forEach(i => {
-            sortedSet.add(i)
+            set.add(i)
         })
-        array.splice(0,array.length)
-        sortedSet.forEach(i => {
-            array.push(i)
+        set.forEach(i => {
+            this.array.push(i)
         })
-        this.array = array
-        this.root = root
+        this.array = mergeSort(this.array)
+        let mid = parseInt((0 + this.array.length)/2)
+        this.root = null
     }
+    #bbst(array, left, right) {
+        if(left > right) {
+            return null
+        }
+        let mid = parseInt((left + right)/2)
+        let root = new Node(array[mid])
+        root.left = this.#bbst(array, left , mid - 1)
+        root.right = this.#bbst(array, mid + 1, right)
+        return root
+    }
+    buildTree(array = this.array) {
+        this.root = this.#bbst(array, 0, array.length - 1)
+        return this.root.data
+    }
+    prettyPrint(root = this.root, prefix = '', isLeft = true) {
+        if (root === null) {
+            return;
+        }
+        if (root.right !== null) {
+            this.prettyPrint(root.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${root.data}`);
+        if (root.left !== null) {
+            this.prettyPrint(root.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+    }
+
 }
---
+++ main.js
@@ -0,0 +1,4 @@
+import { Tree } from "./bst.js"
+let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+test.buildTree()
+test.prettyPrint()
